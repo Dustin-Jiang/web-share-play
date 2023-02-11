6250098e7027dff5e018d5f7e93a57061aac7840
Dustin Jiang <dustinjiang@outlook.com>
2023-02-11 07:47:05 +0000

    feat: add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Routes,Route } from '@solidjs/router';
-import type { Component } from 'solid-js';
+import { Routes, Route } from '@solidjs/router';
+import { Component, lazy } from 'solid-js';
 
-import Home from './pages/home/home';
-import Play from './pages/play/play';
+const Home = lazy(() => import('./pages/home/home'))
+const Play = lazy(() => import("./pages/play/play"));
 
 const App: Component = () => {
   return (
     <Routes>
       <Route path="/" component={Home} />
-      <Route path="/play/:sessionName" component={Play} />
+      <Route path="/:sessionName" component={Play} />
     </Routes>
   );
 };
